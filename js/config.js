e80685e4782241362e2ef111a4c5e0449f43eb4d
Frabragadin <ing.francescobragadin@gmail.com>
2026-04-01 17:37:31 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -11,7 +11,7 @@ const APP_CONFIG = {
   //    Ottieni questi valori dal tuo Google Workspace.
   // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
     // SOLO PER INVIARE SEGNALAZIONI (NON per la mappa)
-appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzii-VQxhwU9uTBUFdblfApxKORAVzKwGsJmEbYnwdr-EmjQOpeNTXUkyPurk_3w3jCGw/exec',
+appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzsgMozEg8kPfnmVwQTxCNe3YSmkCnOBcfYHOXlQwLeXm3_UTgYMgJDDA0jK6-lwUgU7A/exec',
 
 // PER LA MAPPA: usa il CSV pubblicato (NON l'Apps Script)
 sheetsCsvAperte: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTRuFsEGIFOFHPeRKV-3UiSpmyxc1nDXhoOEfL6ZghT0p9vIS26zhNdKbjXUDbWvqR193c2FYHOXlOE/pub?gid=0&single=true&output=csv',
