27685d9ed09da50965562aca684f2f875627a68f
Frabragadin <ing.francescobragadin@gmail.com>
2026-04-02 11:50:50 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -16,6 +16,7 @@ appsScriptUrl: 'https://script.google.com/macros/s/AKfycbysIBNE-yO5ahOVOwUafrSKt
 // PER LA MAPPA: usa il CSV pubblicato (NON l'Apps Script)
 sheetsCsvAperte: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTRuFsEGIFOFHPeRKV-3UiSpmyxc1nDXhoOEfL6ZghT0p9vIS26zhNdKbjXUDbWvqR193c2FYHOXlOE/pub?gid=0&single=true&output=csv',
 sheetsCsvRisolte:'https://docs.google.com/spreadsheets/d/e/2PACX-1vTRuFsEGIFOFHPeRKV-3UiSpmyxc1nDXhoOEfL6ZghT0p9vIS26zhNdKbjXUDbWvqR193c2FYHOXlOE/pub?gid=0&single=true&output=csv',
+sheetsCsvTutte: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTRuFsEGIFOFHPeRKV-3UiSpmyxc1nDXhoOEfL6ZghT0p9vIS26zhNdKbjXUDbWvqR193c2FYHOXlOE/pub?gid=0&single=true&output=csv',  
   
    // 2. IDENTITA' APP
   // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
