9f6ee3b2ab75b8705a31a4f8c7d3129144cf18f9
Frabragadin <ing.francescobragadin@gmail.com>
2026-04-02 09:37:58 +0200

    Update statistiche.js

--- js/statistiche.js
+++ js/statistiche.js
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link rel="canonical" href="https://frabragadin.github.io/segnala-cestini-venezia/" />
+<link rel="canonical" href="https://frabragadin.github.io/Segnala-cestini-venezia/" />
 <meta name="description" content="Statistiche delle segnalazioni di cestini pieni e rifiuti abbandonati nel territorio di Venezia." />
     <meta name="author" content="Francesco Bragadin">
     <meta http-equiv="content-language" content="IT">
@@ -15,41 +15,9 @@
 <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,300;0,400;0,600;0,700;1,300&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/>
 <link rel="stylesheet" href="css/app.css">
-    <link rel="shortcut icon" href="img/favicon.ico"/>
-    <link rel="icon" href="img/favicon.png" type="image/png"/>
-<script src="js/theme.js"></script> 
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-87653723-4"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'UA-87653723-4');
-    </script>
-
-    <!-- Open Graph / Facebook -->
-<meta property="og:type"              content="website" />
-<meta property="og:url"               content="https://frabragadin.github.io/segnala-cestini-venezia/statistiche.html" />
-<meta property="og:site_name"         content="Segnala Cestini - Venezia" />
-<meta property="og:title"             content="Segnala Cestini Venezia | Statistiche" />
-<meta property="og:description"       content="Statistiche delle segnalazioni di cestini pieni e rifiuti abbandonati nel territorio di Venezia." />
-<meta property="og:image"             content="https://frabragadin.github.io/segnala-cestini-venezia/img/og-image.jpg" />
-<meta property="og:image:width"       content="1200" />
-<meta property="og:image:height"      content="630" />
-<meta property="og:image:alt"         content="Segnala Cestini Venezia – statistiche" />
-<meta property="og:locale"            content="it_IT" />
-
-<!-- Twitter Card -->
-<meta name="twitter:card"             content="summary_large_image" />
-<meta name="twitter:site"             content="@ComuneVenezia" />
-<meta name="twitter:creator"          content="@ComuneVenezia" />
-<meta name="twitter:title"            content="Segnala Cestini Venezia | Statistiche" />
-<meta name="twitter:description"      content="Statistiche delle segnalazioni di cestini pieni e rifiuti abbandonati nel territorio di Venezia." />
-<meta name="twitter:image"            content="https://frabragadin.github.io/segnala-cestini-venezia/img/og-image.jpg" />
-<meta name="twitter:image:alt"        content="Segnala Cestini Venezia – statistiche" />
-    
-
+<link rel="shortcut icon" href="img/favicon.ico"/>
+<link rel="icon" href="img/favicon.png" type="image/png"/>
+<script src="js/theme.js"></script>
 </head>
 <body class="page-stats">
 <div class="demo-banner"><i class="fa-solid fa-trash-can"></i> Segnala Cestini - Venezia <button class="demo-banner-close" onclick="this.parentElement.style.display='none'" title="Chiudi"><i class="fa-solid fa-xmark"></i></button></div>
@@ -126,21 +94,8 @@
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2"></script>
 <script src="js/config.js"></script>
 <script src="js/statistiche.js"></script>
-
-<!-- Cookie Banner -->
-<div id="cookieBanner" class="cookie-banner" style="display:none">
-  <div class="cookie-banner__content">
-    <p><i class="fa-solid fa-cookie-bite"></i> Utilizziamo cookie analitici per migliorare il sito. Leggi la <a href="privacy.html">Privacy Policy</a>.</p>
-    <div class="cookie-banner__actions">
-      <button class="cookie-btn cookie-btn--accept" onclick="cookieAccept()">Accetta</button>
-      <button class="cookie-btn cookie-btn--refuse" onclick="cookieRefuse()">Rifiuta</button>
-    </div>
-  </div>
-</div>
-<script src="js/cookie-consent.js"></script>
 
 <!-- BOTTOM NAV mobile -->
 <nav class="bottom-nav">
