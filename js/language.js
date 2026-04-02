18b2875d5c793788f736e431b1d50c0882567f90
Frabragadin <ing.francescobragadin@gmail.com>
2026-04-02 16:54:39 +0200

    Create language.js

---
+++ js/language.js
@@ -0,0 +1,13 @@
+// js/language.js - Scheletro iniziale
+let currentLang = 'it';
+
+function toggleLanguage() {
+  console.log('Lingua cambiata - funzionalità in arrivo');
+  // Qui aggiungeremo le traduzioni più avanti
+}
+
+function initLanguage() {
+  console.log('Language init');
+}
+
+document.addEventListener('DOMContentLoaded', initLanguage);
